9e1dead125e378f9e3f17904c1420697c795135c
Capo_Samu <capotostisamuel@gmail.com>
2023-11-16 19:09:17 +0100

    first commit

---
+++ studio1610.java
@@ -0,0 +1,65 @@
+
+import java.util.Scanner;
+
+public class studio1610 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        
+        System.out.println("Qual è il tuo nome?");
+        String nome = scanner.nextLine();
+        System.out.println("benvenuto "+ nome);
+
+        System.out.println("ora, avrei bisogno del tuo cognome?");
+        String cognome = scanner.nextLine();
+        System.out.println("il tuo cognome è "+ cognome);
+
+        System.out.println("Qual è la tua età?");
+        int eta= scanner.nextInt();
+        scanner.nextLine();
+        System.out.println("la tua età "+ eta);
+
+
+        System.out.println("Dove vivi?");
+        String vivi = scanner.nextLine();
+        System.out.println("vivi a "+ vivi);
+
+        System.out.println("ora, avrei bisogno del tuo codicefiscale?");
+        String codicefiscale = scanner.nextLine();
+        System.out.println("il tuo codice fiscale è "+ codicefiscale);
+        
+       
+        System.out.println("Sei fidanzato? (vero/falso)");
+        String risposta = scanner.nextLine();
+
+        boolean variabileBooleana;
+        
+        
+        if (risposta.equals("si")||risposta.equals("yes")||risposta.equals("vero")) {
+            
+            variabileBooleana = true; } 
+
+        else {variabileBooleana = false;} 
+        System.out.println(variabileBooleana);
+
+        if (variabileBooleana) 
+            {System.out.println("peccato eri cosi carino/a");}
+        
+        else {System.out.println("ti vuoi mettere con me?");
+             }
+        
+        scanner.nextLine();
+
+
+
+
+        
+        
+            
+        }
+        
+        
+}
+       
+    
